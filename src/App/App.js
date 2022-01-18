0dc55dc3017abf382cafc6f528b36977a251678b
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-01-18 23:16:27 +0530

    Using JSX for all non-pure-js React component files

--- src/App/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">I'm the app loader</div>;
-}
-
-export default App;
---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Calculator from "../Apps/Calculator/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      I'm the app loader
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
