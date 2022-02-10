d8952ea5ef2b7ebd9c9a884b25042ad81dab61a0
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-11 04:50:17 +0530

    Safe updation of state

--- src/Apps/MultipleStates/MultiStateSingle.jsx
+++ src/Apps/MultipleStates/MultiStateSingle.jsx
@@ -3,16 +3,33 @@ import React, { useState } from "react";
 function MultiStateSingle() {
   const [state, setState] = useState({ count: 0, reverseCount: 999 });
 
+  // const countClick = () => {
+  //   setState({
+  //       ...state,
+  //       count: state.count + 1,
+  //   });
+  // };
+  // const reverseCountClick = () => {
+  //   setState({
+  //       ...state,
+  //       reverseCount: state.reverseCount - 1,
+  //   });
+  // };
+
   const countClick = () => {
-    setState({
-        ...state,
-        count: state.count + 1,
+    setState((latestState) => {
+      return {
+        ...latestState,
+        count: latestState.count + 1,
+      };
     });
   };
   const reverseCountClick = () => {
-    setState({
-        ...state,
-        reverseCount: state.reverseCount - 1,
+    setState((latestState) => {
+      return {
+        ...latestState,
+        reverseCount: latestState.reverseCount - 1,
+      };
     });
   };
 
