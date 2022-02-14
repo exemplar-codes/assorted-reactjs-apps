8cf5012bf219e5b052702936038a477e4d31169d
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-14 08:52:51 +0530

    Demo tagged styled in styled-components

--- src/Apps/StyledComponentsDemo/Div.jsx
+++ src/Apps/StyledComponentsDemo/Div.jsx
@@ -3,8 +3,16 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledDiv = styled.div`
+  // for the current tag only
   color: red;
   font-size: 64px;
+
+  // &:hover - pseudo classes
+
+  div {
+    // for tags inside
+    color: blue;
+  }
 `;
 
 function Div(props) {
--- src/Apps/StyledComponentsDemo/StyledComponentsDemo.jsx
+++ src/Apps/StyledComponentsDemo/StyledComponentsDemo.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Div from "./Div";
 
 function StyledComponentsDemo() {
-  return <Div>Hi, I am Sanjar</Div>;
+  return (
+    <Div>
+      Hi, I am Sanjar23<Div>23 <div>23</div></Div>
+    </Div>
+  );
 }
 
 export default StyledComponentsDemo;
