577aa48e9d8d7d45beace3b1a46481146fb6e3cb
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-03-04 21:56:58 +0530

    Cannot use useRef variable in JSX

--- src/Apps/UseRefDemo/UseRefMain.jsx
+++ src/Apps/UseRefDemo/UseRefMain.jsx
@@ -4,14 +4,14 @@ import "./UseRefMain.css";
 
 import UseRefNeed1 from "./UseRefNeed1";
 import UseRefDerivation2 from "./UseRefDerivation2";
-import UseRefDemo4StateFlipper from "./UseRefDemo4StateFlipper";
+import UseRefDemo3State from "./UseRefDemo3State";
 
 function UseRefMain() {
   return (
     <div className="container">
       <UseRefNeed1 />
       <UseRefDerivation2 />
-      <UseRefDemo4StateFlipper />
+      <UseRefDemo3State />
     </div>
   );
 }
