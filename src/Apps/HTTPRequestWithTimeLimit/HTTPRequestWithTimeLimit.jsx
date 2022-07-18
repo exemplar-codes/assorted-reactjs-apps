a011d80127b718f0c87b4e0464f5e15111397e03
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-07-18 22:10:43 +0530

    Elegant time bound request in React

--- src/Apps/HTTPRequestWithTimeLimit/HTTPRequestWithTimeLimit.jsx
+++ src/Apps/HTTPRequestWithTimeLimit/HTTPRequestWithTimeLimit.jsx
@@ -12,23 +12,32 @@ function HTTPRequestWithTimeLimit() {
 
     // Need to run both timer and fetch asynchronously. Stop if timer exceeds, or continue.
     // Solution make a new promise, and call reject if timer exceeds
+
+    // We are using a timer for the fetch to, to simulate throttling
+    // The issue is both run, which is bad behavior. i.e. we should clean the other timer if one runs
+    // I'll clear both, if one of them has run, it would have already been cleared.
+    let fetchTimer, limitTimer; // to keep track of the timers
     new Promise((resolve, reject) => {
-      fetch("https://api.github.com/users/sanjarcode")
-        .then((response) => response.json())
-        .then((data) => {
-          setTimeout(() => {
+      fetchTimer = setTimeout(() => {
+        fetch("https://api.github.com/users/sanjarcode")
+          .then((response) => response.json())
+          .then((data) => {
             setData(data);
             setIsLoading(false);
             resolve(); // everything OK
-          }, 1000);
-        });
+          });
+      }, 1000);
 
-      setTimeout(() => {
+      limitTimer = setTimeout(() => {
         reject(new Error("Time Limit Exceeded"));
-      }, 300);
+      }, 30);
     })
       .catch(() => setRequestTimeLimitExceeded(true))
-      .finally(() => setIsLoading(false));
+      .finally(() => {
+        clearTimeout(limitTimer);
+        clearTimeout(fetchTimer);
+        setIsLoading(false);
+      });
   }
 
   return (
@@ -41,8 +50,18 @@ function HTTPRequestWithTimeLimit() {
       </p>
       {!loading && data && (
         <h2>
-          Reason there's no need for useEffect - the HTTP request was triggered
-          manually and not on page load
+          Create a 'void' new promise and run both fetch and timer. Resolve
+          inside fetch, reject with an error inside timer. In the catch block of
+          this void promise, set timeLimitExceededState to true. And in the
+          finally block, clear the timer using window.clearTimeout (so it's not
+          run if fetch is successful)
+          <br />
+          Wrap this void promise in a function and call it inside useEffect or
+          as a user event callback.
+          <br />
+          <a href="https://stackoverflow.com/a/53350139/11392807">
+            Inspiration
+          </a>
         </h2>
       )}
     </>
