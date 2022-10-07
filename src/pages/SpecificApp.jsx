1aa51744e04a7f1138dc8c917f865af99b997488
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-10-08 03:06:10 +0530

    Add back link for invalid app

--- src/pages/SpecificApp.jsx
+++ src/pages/SpecificApp.jsx
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AppList from "../Apps/AppList";
 
 function SpecificApp() {
   const { appName } = useParams();
 
-  return AppList[appName]?.();
+  return (
+    AppList[appName]?.() || (
+      <>
+        <p>
+          No app named <code>{appName}</code>.
+        </p>
+        <Link to="/apps">Go back</Link>
+      </>
+    )
+  );
 }
 
 export default SpecificApp;
